6d11b3a7b3b3bb81156b6f6e630dcfdeebdeff4c
Himmu9131 <114733178+Himmu9131@users.noreply.github.com>
2024-12-18 10:15:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function capitalizeFName() {
+    const inputField = document.getElementById("fname");
+    const value = inputField.value;
+
+    // Capitalize the value
+    inputField.value = value.toUpperCase(); // Change to .toLowerCase() if lowercase required
+}
+const inputField = document.getElementById("fname");
+inputField.addEventListener("blur", capitalizeFName);
